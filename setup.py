417e592d1e6925a2c419d45f834e12349bd942f3
Aart Bik <ajcbik@google.com>
2024-06-13 14:53:33 -0700

    [mpact][compiler] minor comment cleanup

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
-# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-# See https://llvm.org/LICENSE.txt for license information.
-# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-# Also available under a BSD-style license. See LICENSE.
-
 # Script for generating the mpact wheel.
 # ```
 # $ python setup.py bdist_wheel
@@ -28,6 +23,7 @@
 # directory that our CMake build produces. We go through quite a bit of effort
 # on the CMake side to organize that directory already, so we avoid duplicating
 # that here, and just package up its contents.
+
 import os
 import pathlib
 import shutil
