68f8a6619a62bd097fa9e1d88dcb07185a3c1c87
Aart Bik <ajcbik@google.com>
2024-06-14 15:50:46 -0700

    [mpact][compiler] add normalization and scaling test (#33)
    * [mpact][compiler] enable vectorization (SIMD)
    
    * [mpact][compiler] add normalization and scaling test
    
    Note, this still needs torch-mlir work to be fully functional
    
    * lint scale

--- test/python/lif.py
+++ test/python/lif.py
@@ -3,6 +3,7 @@
 import torch
 
 from mpact.mpactbackend import mpact_jit, mpact_jit_compile, mpact_jit_run
+
 from mpact.models.lif import Block
 
 net = Block()
--- test/python/spmv.py
+++ test/python/spmv.py
@@ -3,6 +3,7 @@
 import torch
 
 from mpact.mpactbackend import mpact_jit, mpact_jit_compile, mpact_jit_run
+
 from mpact.models.kernels import MVNet
 
 net = MVNet()
